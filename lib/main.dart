e301405fe78ea903411dcdcc91e757460246001c
lukypurdiono <lukypurdiono@gmail.com>
2021-07-29 10:26:52 +0700

    AppBar with Gradient

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_erico_1/login_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,7 +9,44 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.menu),
+              );
+            },
+          ),
+          title: Text('AppBar Gradient'),
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.settings),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.exit_to_app),
+            ),
+          ],
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xff0096ff), Color(0xff6610f2)],
+                begin: FractionalOffset.topLeft,
+                end: FractionalOffset.bottomRight,
+              ),
+              image: DecorationImage(
+                image: NetworkImage(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZJGCJjPIDPohfSQfm-m7lS07j__IAEIu0idk_-H9hm3aSdEcaSSvjtu28PLXXw1qEdAY&usqp=CAU'),
+                fit: BoxFit.none,
+                repeat: ImageRepeat.repeat,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
